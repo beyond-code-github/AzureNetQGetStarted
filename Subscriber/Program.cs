b8c3c8a23c2deff582dde538118364ef9812b9ad
Pete Smith <roysvork@gmail.com>
2014-06-25 20:29:28 +0100

    Would rather not admit the reason for my own stupidity

--- Subscriber/Program.cs
+++ Subscriber/Program.cs
@@ -9,20 +9,22 @@
         static void Main(string[] args)
         {
             const string connectionstring =
-                   "Endpoint=sb://servicebus/ServiceBusDefaultNamespace;StsEndpoint=https://servicebus:10355/ServiceBusDefaultNamespace;RuntimePort=10354;ManagementPort=10355";
+                "Endpoint=sb://servicebus/ServiceBusDefaultNamespace;StsEndpoint=https://servicebus:10355/ServiceBusDefaultNamespace;RuntimePort=10354;ManagementPort=10355";
 
             using (var bus = AzureBusFactory.CreateBus(connectionstring))
             {
-                var input = "";
-                Console.WriteLine("Enter a message. 'Quit' to quit.");
-                while ((input = Console.ReadLine()) != "Quit")
-                {
-                    bus.Publish(new TextMessage
-                    {
-                        Text = input
-                    });
-                }
+                bus.Subscribe<TextMessage>(HandleTextMessage);
+
+                Console.WriteLine("Listening for messages. Hit <return> to quit.");
+                Console.ReadLine();
             }
+        }
+
+        static void HandleTextMessage(TextMessage textMessage)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Got message: {0}", textMessage.Text);
+            Console.ResetColor();
         }
     }
 }
