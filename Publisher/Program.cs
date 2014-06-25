ec1e7a340fe3e50796b85a28cd2cac68c49a9184
Pete Smith <roysvork@gmail.com>
2014-06-25 20:26:30 +0100

    Initial commit

---
+++ Publisher/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Publisher
+{
+    using System;
+    using AzureNetQ;
+    using Messages;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionstring =
+                "Endpoint=sb://servicebus/ServiceBusDefaultNamespace;StsEndpoint=https://servicebus:10355/ServiceBusDefaultNamespace;RuntimePort=10354;ManagementPort=10355";
+
+            using (var bus = AzureBusFactory.CreateBus(connectionstring))
+            {
+                bus.Subscribe<TextMessage>(HandleTextMessage);
+
+                Console.WriteLine("Listening for messages. Hit <return> to quit.");
+                Console.ReadLine();
+            }
+        }
+
+        static void HandleTextMessage(TextMessage textMessage)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Got message: {0}", textMessage.Text);
+            Console.ResetColor();
+        }
+    }
+}
---
+++ Subscriber/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Subscriber
+{
+    using System;
+    using AzureNetQ;
+    using Messages;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionstring =
+                   "Endpoint=sb://servicebus/ServiceBusDefaultNamespace;StsEndpoint=https://servicebus:10355/ServiceBusDefaultNamespace;RuntimePort=10354;ManagementPort=10355";
+
+            using (var bus = AzureBusFactory.CreateBus(connectionstring))
+            {
+                var input = "";
+                Console.WriteLine("Enter a message. 'Quit' to quit.");
+                while ((input = Console.ReadLine()) != "Quit")
+                {
+                    bus.Publish(new TextMessage
+                    {
+                        Text = input
+                    });
+                }
+            }
+        }
+    }
+}
